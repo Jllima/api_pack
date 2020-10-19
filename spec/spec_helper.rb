48a2f08b5bd073073c7c04e49fe7706a7ef6b7bf
jorge <jlimajorge@gmail.com>
2020-10-19 16:54:20 -0300

    chore: config coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,6 @@ require 'bundler/setup'
 require 'simplecov'
 require 'coveralls'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::HTMLFormatter,
-                                                                Coveralls::SimpleCov::Formatter])
 SimpleCov.start do
   add_filter '/spec/'
   add_filter '/vendor/'
